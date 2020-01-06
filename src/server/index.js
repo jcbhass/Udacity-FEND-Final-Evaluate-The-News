6eb11efa2455120bc0cc35ed01f02430cd004111
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-01-06 13:54:18 -0500

    Added express, cors, body-parser, and webpack

--- src/server/index.js
+++ src/server/index.js
@@ -3,6 +3,16 @@ const express = require('express')
 const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
+const bodyParser = require('body-parser');
+/* Middleware*/
+//Here we are configuring express to use body-parser as middle-ware.
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+// Initialize the main project folder
 
 app.use(express.static('src/client'))
 
