bfece3964f80cada740c70f87cd863005fbf76e7
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-01-06 12:58:57 -0500

    Starting from original project to make sure I have everything installed properly

--- src/server/index.js
+++ src/server/index.js
@@ -4,17 +4,17 @@ const mockAPIResponse = require('./mockAPI.js')
 
 const app = express()
 
-app.use(express.static('dist'))
+app.use(express.static('src/client'))
 
 console.log(__dirname)
 
 app.get('/', function (req, res) {
-    res.sendFile('dist/index.html')
+    res.sendFile('/client/views/index.html', { root: __dirname + '/..' })
 })
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+    console.log('Example app listening on port 3000!')
 })
 
 app.get('/test', function (req, res) {
