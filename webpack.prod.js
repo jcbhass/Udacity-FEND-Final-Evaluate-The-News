f2bcb41947e76cef95a834fc2edfb4fd84fd6b00
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-01-06 19:00:40 -0500

    Added sass, cleanwebpack plugin, and set up web-dev-server

--- webpack.prod.js
+++ webpack.prod.js
@@ -4,14 +4,19 @@ const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 
 module.exports = {
+    mode: 'production',
     entry: './src/client/index.js',
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            }
+            },
+            {
+                test: /\.scss$/,
+                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+            }            
         ]
      },
     plugins: [
