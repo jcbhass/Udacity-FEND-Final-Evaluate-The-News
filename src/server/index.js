48c036abbc7fbdb432eab8a813989109941b057c
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-01-19 14:32:38 -0500

    HtmlWebPackPlugin

--- src/server/index.js
+++ src/server/index.js
@@ -11,7 +11,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
 
-app.use(express.static('src/client'))
+app.use(express.static('dist'))
 
 console.log(__dirname)
 
