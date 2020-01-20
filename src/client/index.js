c55e6afd9b6fc23d9973ff9419b990c4133cb020
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-01-20 14:24:02 -0500

    Scss showed and submit button worked.

--- src/client/index.js
+++ src/client/index.js
@@ -1,7 +1,19 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
+import './styles/resets.scss'
+import './styles/header.scss'
+import './styles/form.scss'
+import './styles/footer.scss'
+import './styles/base.scss'
+
 console.log(checkForName);
+console.log(handleSubmit);
 
 alert("I EXIST")
 console.log("CHANGE!!");
+
+export {
+    checkForName, 
+    handleSubmit
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -8,6 +8,10 @@ module.exports = {
     mode: 'development',
     devtool: 'source-map',
     stats: 'verbose',
+    output: {
+        libraryTarget: 'var',
+        library: 'Client' //Where custom javascript goes
+    },
     module: {
         rules: [
             {
@@ -19,7 +23,17 @@ module.exports = {
                 test: /\.scss$/,
                 use: [ 'style-loader', 'css-loader', 'sass-loader' ]
             }
-            
+            // {
+            //     test: /\.s[ac]ss$/i,
+            //     use: [
+            //       // Creates `style` nodes from JS strings
+            //       'style-loader',
+            //       // Translates CSS into CommonJS
+            //       'css-loader',
+            //       // Compiles Sass to CSS
+            //       'sass-loader',
+            //     ],
+            //   },        
         ]
     },
     plugins: [
