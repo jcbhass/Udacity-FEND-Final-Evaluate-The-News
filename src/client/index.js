ddb28d40d13665dd377bde70c27460375b4a2f0b
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-01-20 11:30:55 -0500

    8th time round

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,7 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
 
-import './styles/resets.scss'
-import './styles/header.scss'
-import './styles/form.scss'
-import './styles/footer.scss'
-import './styles/base.scss'
-
 console.log(checkForName);
 
-alert("I Exist")
-console.log("Hi!!");
+alert("I EXIST")
+console.log("CHANGE!!");
--- webpack.dev.js
+++ webpack.dev.js
@@ -11,14 +11,10 @@ module.exports = {
     module: {
         rules: [
             {
-                test: /\.js$/,
+                test: '/\.js$/',
                 exclude: /node_modules/,
                 loader: "babel-loader"
-            },
-            {
-                test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-            }            
+            }
         ]
     },
     plugins: [
