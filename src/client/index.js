1b160df4c7766a1869ad1c84ac2c259ad272e605
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-01-19 20:48:30 -0500

    Sass install and style changes

--- src/client/index.js
+++ src/client/index.js
@@ -1,4 +1,10 @@
 import { handleSubmit } from './js/formHandler'
 import { checkForName } from './js/nameChecker'
+import './styles/base.scss'
+import './styles/footer.scss'
+import './styles/form.scss'
+import './styles/header.scss'
+import './styles/resets.scss'
+
 
 alert('Jennifer!')
