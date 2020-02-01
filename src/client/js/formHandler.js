bd26a94724afb4fda8f1f7931ea0151eed3015d9
jcbhass <47954686+jcbhass@users.noreply.github.com>
2020-02-01 13:03:36 -0500

    Installed Minifiers

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -13,11 +13,11 @@ function handleSubmit(event) {
     var name = document.getElementById('name').value;
     axios.post('http://localhost:8080/article', {articleUrl: name})
     .then(function(res) {
+        console.log(res);
         const data = res.data;
         document.getElementById('polarity').innerHTML = data.polarity;
         document.getElementById('subjectivity').innerHTML = data.subjectivity_confidence;
         document.getElementById('text').innerHTML = data.text;
-        console.log(res);
     })
   
 }
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,10 +1,16 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+const TerserPlugin = require('terser-webpack-plugin')
 
 module.exports = {
     entry: './src/client/index.js',
     mode: 'production',
+    optimization: {
+        minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})]
+    },
     module: {
         rules: [
             {
@@ -14,7 +20,7 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ] //style-loader made css styles inline, which is slower
             }
         ]
     }, 
@@ -22,6 +28,7 @@ module.exports = {
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new MiniCssExtractPlugin({filename: '[name].css'})
     ]
 }
